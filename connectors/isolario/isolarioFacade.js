8698763465c4a856210efb67c8018f5473322514
Massimo Candela <nve@webrobotics.net>
2015-09-25 09:17:05 +0200

    exposed appUrl

--- connectors/isolario/isolarioFacade.js
+++ connectors/isolario/isolarioFacade.js
@@ -253,7 +253,7 @@ var StreamingFacade = function(environment){
         $this.options = options;
 
         socket = new IsolarioWebSocket({
-            app_url : "ws://146.48.78.3:9999/my_subnet_reachability",
+            app_url : environment.defaultParams.streamingServerUrl || "ws://146.48.78.3:9999/my_subnet_reachability",
             handle_message : function(data){$this._handleMessage.call($this, data)},
             post_send_app : function(data){$this._postConnection.call($this, data)},
             token: environment.defaultParams.authenticationToken
